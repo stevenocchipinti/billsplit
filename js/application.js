70a902dc71ee3fa94f0594bad126201c9d522641
Steven Occhipinti <steve.occhipinti@rea-group.com>
2013-05-20 23:44:24 +1000

    Add the basic functionality of the calculator

--- js/application.js
+++ js/application.js
@@ -5,7 +5,7 @@ angular.module('billSplit', []).
         controller:EntryCtrl,
         templateUrl:'entry.html'
       }).
-      when('/calculator/:people', {
+      when('/calculator/:numberOfPeople', {
         controller:CalculatorCtrl,
         templateUrl:'calculator.html'
       }).
@@ -22,5 +22,38 @@ function EntryCtrl($scope, $location) {
 
 
 function CalculatorCtrl($scope, $location, $routeParams) {
-  console.log("Need to make " + $routeParams.people + " people");
+  $scope.people = {};
+  for(var i=1 ; i<=$routeParams.numberOfPeople ; i++) {
+    $scope.people[i] = {
+      id: i,
+      name: "Person " + i,
+      items: [],
+      itemCount: function() {
+        var count = 0;
+        angular.forEach(this.items, function(item) { count++ });
+        return count;
+      },
+      total: function() {
+        var total = 0.00;
+        angular.forEach(this.items, function(item) { total += item.price });
+        return total;
+      }
+    };
+  }
+
+  $scope.addItemToPeople = function() {
+    angular.forEach($('input.person[type="checkbox"]:checked'), function(person) {
+      $scope.people[person.id].items.push({
+        name: $('#inputItemName'),
+        price: parseFloat($('#inputItemPrice').val())
+      });
+    });
+    $scope.resetFields();
+  };
+
+  $scope.resetFields = function() {
+    $('#inputItemName').val('');
+    $('#inputItemPrice').val('1');
+    $('input.person[type="checkbox"]').prop("checked", false);
+  }
 }
